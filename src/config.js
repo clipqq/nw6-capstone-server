46c4a013880d11e47a6dd319cb9fdaa61c32e70d
Thersis94 <thersis94@gmail.com>
2020-01-30 21:14:31 -0700

    Updating all references of old database name

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  DB_URL: process.env.DB_URL,
+  DATABASE: process.env.DATABASE_URL,
   TEST_DB_URL: process.env.TEST_DB_URL,
   JWT_SECRET: process.env.JWT_SECRET || "change-this-secret",
   JWT_EXPIRY: process.env.JWT_EXPIRY || "24h"
